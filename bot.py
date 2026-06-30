3b6d1d2b374ae6d56e6170a5478c76c8229d3554
shohjahonash2011-droid <shohjahon.ash.2011@gmail.com>
2026-06-30 16:47:01 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from telegram.ext import (
 
 import os
 
-BOT_TOKEN = os.getenv("8811406011:AAGJWa3oZruncw0TEWI_tcsnBl3UyiPb57k")
+BOT_TOKEN = os.getenv"8811406011:AAGJWa3oZruncw0TEWI_tcsnBl3UyiPb57k"
 
 # Hozircha bo'sh
 ANIME_CODES = {}
